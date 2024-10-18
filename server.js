4ffa2bbf8ed6d47a8331a766787739af72df1556
adarsh02 <aadarshraazz007@gmail.com>
2024-10-18 21:15:37 +0530

    added middleware for log request

--- server.js
+++ server.js
@@ -4,8 +4,24 @@ import routes from "./routes/routes.js";
 const app = express();
 const PORT = 5000;
 
+const logRequests = (req, res, next) => {
+  res.on("finish", () => {
+    const method = req.method;
+    const url = req.url;
+    const statusCode = res.statusCode;
+
+    console.log(` ${method} ${url} - Status: ${statusCode} -`);
+  });
+
+  next();
+};
+
+app.use(logRequests);
+
+app.use(express.json());
+
 app.use("/api", routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
