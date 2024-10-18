a5e09e1440ca5d2536b985835f38fc6ed52e54ac
adarsh02 <aadarshraazz007@gmail.com>
2024-10-18 16:11:55 +0530

    used api routes to get games data

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import routes from "./routes/routes.js";
 
 const app = express();
 const PORT = 5000;
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
