540fdf0a5c2e929e52ec0554cf540d08161f4c23
adarsh02 <aadarshraazz007@gmail.com>
2024-10-18 16:17:03 +0530

    added routes to get games by id

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,13 @@ router.get("/games", (req, res) => {
   res.status(200).json(games);
 });
 
+router.get("/games/:id", (req, res) => {
+  const game = games.find((g) => g.id === req.params.id);
+  if (game) {
+    res.status(200).json(game);
+  } else {
+    res.status(404).json({ message: "Game not found" });
+  }
+});
+
 export default router;
